56216daae78fe29d8f8943758d2f58e63a2f36a0
Elzbieta Siwy <es1911@ic.ac.uk>
2014-01-17 20:55:30 +0100

    Initialised the GWT project

---
+++ github/src/github/shared/FieldVerifier.java
@@ -0,0 +1,10 @@
+package github.shared;
+
+public class FieldVerifier {
+	public static boolean isValidName(String name) {
+		if (name == null) {
+			return false;
+		}
+		return name.length() > 3;
+	}
+}
